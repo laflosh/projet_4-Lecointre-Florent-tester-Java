0f795b9715d3d3690819d4152d21743f23236b8f
laflosh <l_florent_l@hotmail.fr>
2024-07-23 18:23:33 +0200

    Ecriture test fonctionnallitée 30min gratuite

--- src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
+++ src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
@@ -144,5 +144,39 @@ public class FareCalculatorServiceTest {
         
         assertEquals( (24 * Fare.CAR_RATE_PER_HOUR) , ticket.getPrice());
     }
+    
+    @Test
+    public void calculateFareCarWithLessThan30minutesParkingTimeDescription(){
+    	
+    	Date inTime = new Date();
+    	inTime.setTime(System.currentTimeMillis() - (30 * 60 * 1000));//30 minutes parking time should give free fare 
+    	Date outTime = new Date();
+    	ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR,false);
+    	
+    	ticket.setInTime(inTime);
+    	ticket.setOutTime(outTime);
+    	ticket.setParkingSpot(parkingSpot);
+    	fareCalculatorService.calculateFare(ticket);
+    	
+    	assertEquals(0 , ticket.getPrice());
+    	
+    }
+    
+    @Test
+    public void calculateFareBikeWithLessThan30minutesParkingTimeDescription(){
+    	
+    	Date inTime = new Date();
+    	inTime.setTime(System.currentTimeMillis() - (30 * 60 * 1000));//30 minutes parking time should give free fare 
+    	Date outTime = new Date();
+    	ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.BIKE,false);
+    	
+    	ticket.setInTime(inTime);
+    	ticket.setOutTime(outTime);
+    	ticket.setParkingSpot(parkingSpot);
+    	fareCalculatorService.calculateFare(ticket);
+    	
+    	assertEquals(0, ticket.getPrice());
+    	
+    }
 
 }
