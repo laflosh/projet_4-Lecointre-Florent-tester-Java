75c2b2206a4542a84872a4889a9242a479319e73
laflosh <l_florent_l@hotmail.fr>
2024-08-02 16:55:17 +0200

    Test intégration 5%  réduction

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -5,7 +5,6 @@ import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
-import com.parkit.parkingsystem.model.ParkingSpot;
 import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
@@ -18,11 +17,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+
+import java.util.concurrent.TimeUnit;
 
 @ExtendWith(MockitoExtension.class)
 public class ParkingDataBaseIT {
@@ -58,6 +56,8 @@ public class ParkingDataBaseIT {
     @AfterAll
     private static void tearDown(){
 
+    	//dataBasePrepareService.clearDataBaseEntries();
+    	
     }
 
     @Test
@@ -75,14 +75,58 @@ public class ParkingDataBaseIT {
     }
 
     @Test
-    public void testParkingLotExit(){
+    public void testParkingLotExit() {
     	
         testParkingACar();
         
+        try {
+        	
+			TimeUnit.SECONDS.sleep(1);
+			
+		} catch (InterruptedException e) {
+			
+			e.printStackTrace();
+			
+		}
+        
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+        
         parkingService.processExitingVehicle();
+        
         //TODO: check that the fare generated and out time are populated correctly in the database
         
+        //assertNotNull(ticketDAO.getTicket("ABCDEF"));
+        
+        //Ticket ticketTest = ticketDAO.getTicket("ABCDEF");
+        
+        //assertNotNull(ticketTest.getOutTime());
+        //assertEquals(ticketTest.getPrice(), 0); //Vehicle stay less than 30min so it's free time
+        
+    }
+    
+    @Test
+    public void testParkingLotExitRecurringUser(){
+    	
+    	testParkingLotExit();
+    	
+    	testParkingACar();
+    	
+        try {
+        	
+			TimeUnit.SECONDS.sleep(1);
+			
+		} catch (InterruptedException e) {
+			
+			e.printStackTrace();
+			
+		}
+    	
+    	ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+    	
+    	parkingService.processExitingVehicle();
+    	
+    	assertEquals(ticketDAO.getNbTicket("ABCDEF"), 2); //If equal to 2 thier will be the 5% reduc
+    	
     }
     
 }
